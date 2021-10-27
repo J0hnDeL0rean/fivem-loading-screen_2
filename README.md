931d92fedbc44b59d454eb52da764db1982c12bc
J0hnDeL0rean <lauriszproductions@gmail.com>
2021-10-27 18:45:37 +0300

    main core

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# fivem-loading-screen_2
- 
+Live preview : 
+
+To change the picture edit the 79th line of "script.js".
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/K3K36LXOH)
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "bodacious"
+game "gta5"
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown "yes"
+
+files {
+    "index.html",
+	"style.css",
+	"script.js",
+	"music.mp3"
+}
+
+client_script 'client.lua'
